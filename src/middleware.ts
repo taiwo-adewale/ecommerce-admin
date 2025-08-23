ae444cb4d9b0e7c66bdeb1345b607f99b5af8981
Taiwo Adewale <adewaletaiwo08@gmail.com>
2025-08-23 22:21:44 +0100

    reactivate middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,31 +3,31 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
-  // const supabase = createMiddlewareClient({ req, res });
+  const supabase = createMiddlewareClient({ req, res });
 
-  // const {
-  //   data: { session },
-  // } = await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
-  // const authRoutes = [
-  //   "/login",
-  //   "/signup",
-  //   "/forgot-password",
-  //   "/update-password",
-  // ];
+  const authRoutes = [
+    "/login",
+    "/signup",
+    "/forgot-password",
+    "/update-password",
+  ];
 
-  // const isAuthRoute = authRoutes.includes(req.nextUrl.pathname);
-  // const isLoggedIn = !!session?.user;
+  const isAuthRoute = authRoutes.includes(req.nextUrl.pathname);
+  const isLoggedIn = !!session?.user;
 
-  // if (isLoggedIn && isAuthRoute) {
-  //   return NextResponse.redirect(new URL("/", req.url));
-  // }
+  if (isLoggedIn && isAuthRoute) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 
-  // if (!isLoggedIn && !isAuthRoute) {
-  //   const loginUrl = new URL("/login", req.url);
-  //   loginUrl.searchParams.set("redirect_to", req.nextUrl.pathname);
-  //   return NextResponse.redirect(loginUrl);
-  // }
+  if (!isLoggedIn && !isAuthRoute) {
+    const loginUrl = new URL("/login", req.url);
+    loginUrl.searchParams.set("redirect_to", req.nextUrl.pathname);
+    return NextResponse.redirect(loginUrl);
+  }
 
   return res;
 }
