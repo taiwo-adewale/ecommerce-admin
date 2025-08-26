228724545ce860f363806162788b708fe055b569
Taiwo Adewale <adewaletaiwo08@gmail.com>
2025-08-26 05:17:40 +0100

    add RBAC for products table features

--- src/app/(dashboard)/products/_components/ProductActions.tsx
+++ src/app/(dashboard)/products/_components/ProductActions.tsx
@@ -15,78 +15,88 @@ import { editProducts } from "@/actions/products/editProducts";
 import { deleteProducts } from "@/actions/products/deleteProducts";
 import { exportProducts } from "@/actions/products/exportProducts";
 import { RowSelectionProps } from "@/types/data-table";
+import { useAuthorization } from "@/hooks/use-authorization";
 
 export default function ProductActions({
   rowSelection,
   setRowSelection,
 }: RowSelectionProps) {
-  const handleBulkUpdate = () => {};
+  const { hasPermission } = useAuthorization();
 
   return (
     <Card className="mb-5">
       <div className="flex flex-col xl:flex-row xl:justify-between gap-4">
         <ExportDataButtons action={exportProducts} tableName="products" />
 
-        <div className="flex flex-col sm:flex-row gap-4">
-          <ProductBulkActionSheet
-            action={(formData) =>
-              editProducts(Object.keys(rowSelection), formData)
-            }
-            onSuccess={() => setRowSelection({})}
-          >
-            <SheetTrigger asChild>
-              <Button
-                variant="secondary"
-                size="lg"
-                type="button"
-                disabled={!Boolean(Object.keys(rowSelection).length)}
-                className="sm:flex-grow xl:flex-grow-0 transition-opacity duration-300"
-                onClick={handleBulkUpdate}
+        {(hasPermission("products", "canEdit") ||
+          hasPermission("products", "canDelete") ||
+          hasPermission("products", "canCreate")) && (
+          <div className="flex flex-col sm:flex-row gap-4">
+            {hasPermission("products", "canEdit") && (
+              <ProductBulkActionSheet
+                action={(formData) =>
+                  editProducts(Object.keys(rowSelection), formData)
+                }
+                onSuccess={() => setRowSelection({})}
               >
-                <PenSquare className="mr-2 size-4" /> Bulk Action
-              </Button>
-            </SheetTrigger>
-          </ProductBulkActionSheet>
+                <SheetTrigger asChild>
+                  <Button
+                    variant="secondary"
+                    size="lg"
+                    type="button"
+                    disabled={!Boolean(Object.keys(rowSelection).length)}
+                    className="sm:flex-grow xl:flex-grow-0 transition-opacity duration-300"
+                  >
+                    <PenSquare className="mr-2 size-4" /> Bulk Action
+                  </Button>
+                </SheetTrigger>
+              </ProductBulkActionSheet>
+            )}
 
-          <ActionAlertDialog
-            title={`Delete ${Object.keys(rowSelection).length} products?`}
-            description="This action cannot be undone. This will permanently delete the products and their associated data from the database."
-            actionButtonText="Delete Products"
-            toastSuccessMessage="Products deleted successfully"
-            queryKey="products"
-            action={() => deleteProducts(Object.keys(rowSelection))}
-            onSuccess={() => setRowSelection({})}
-          >
-            <Button
-              variant="destructive"
-              size="lg"
-              type="button"
-              disabled={!Boolean(Object.keys(rowSelection).length)}
-              className="sm:flex-grow xl:flex-grow-0 transition-opacity duration-300"
-            >
-              <Trash2 className="mr-2 size-4" />
-              Delete
-            </Button>
-          </ActionAlertDialog>
-
-          <ProductFormSheet
-            title="Add Product"
-            description="Add necessary product information here"
-            submitButtonText="Add Product"
-            actionVerb="added"
-            action={addProduct}
-          >
-            <SheetTrigger asChild>
-              <Button
-                variant="default"
-                size="lg"
-                className="sm:flex-grow xl:flex-grow-0"
+            {hasPermission("products", "canDelete") && (
+              <ActionAlertDialog
+                title={`Delete ${Object.keys(rowSelection).length} products?`}
+                description="This action cannot be undone. This will permanently delete the products and their associated data from the database."
+                actionButtonText="Delete Products"
+                toastSuccessMessage="Products deleted successfully"
+                queryKey="products"
+                action={() => deleteProducts(Object.keys(rowSelection))}
+                onSuccess={() => setRowSelection({})}
               >
-                <Plus className="mr-2 size-4" /> Add Product
-              </Button>
-            </SheetTrigger>
-          </ProductFormSheet>
-        </div>
+                <Button
+                  variant="destructive"
+                  size="lg"
+                  type="button"
+                  disabled={!Boolean(Object.keys(rowSelection).length)}
+                  className="sm:flex-grow xl:flex-grow-0 transition-opacity duration-300"
+                >
+                  <Trash2 className="mr-2 size-4" />
+                  Delete
+                </Button>
+              </ActionAlertDialog>
+            )}
+
+            {hasPermission("products", "canCreate") && (
+              <ProductFormSheet
+                title="Add Product"
+                description="Add necessary product information here"
+                submitButtonText="Add Product"
+                actionVerb="added"
+                action={addProduct}
+              >
+                <SheetTrigger asChild>
+                  <Button
+                    variant="default"
+                    size="lg"
+                    className="sm:flex-grow xl:flex-grow-0"
+                  >
+                    <Plus className="mr-2 size-4" /> Add Product
+                  </Button>
+                </SheetTrigger>
+              </ProductFormSheet>
+            )}
+          </div>
+        )}
       </div>
     </Card>
   );
--- src/app/(dashboard)/products/_components/products-table/columns.tsx
+++ src/app/(dashboard)/products/_components/products-table/columns.tsx
@@ -22,166 +22,189 @@ import { SkeletonColumn } from "@/types/skeleton";
 import { editProduct } from "@/actions/products/editProduct";
 import { deleteProduct } from "@/actions/products/deleteProduct";
 import { toggleProductPublishedStatus } from "@/actions/products/toggleProductStatus";
+import { HasPermission } from "@/hooks/use-authorization";
 
-export const columns: ColumnDef<Product>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-  },
-  {
-    header: "product name",
-    cell: ({ row }) => (
-      <div className="flex gap-2 items-center">
-        <ImagePlaceholder
-          src={row.original.image_url}
-          alt={row.original.name}
-          width={32}
-          height={32}
-          className="size-8 rounded-full"
-        />
+export const getColumns = ({
+  hasPermission,
+}: {
+  hasPermission: HasPermission;
+}) => {
+  const columns: ColumnDef<Product>[] = [
+    {
+      header: "product name",
+      cell: ({ row }) => (
+        <div className="flex gap-2 items-center">
+          <ImagePlaceholder
+            src={row.original.image_url}
+            alt={row.original.name}
+            width={32}
+            height={32}
+            className="size-8 rounded-full"
+          />
 
-        <Typography className="capitalize block truncate">
-          {row.original.name}
-        </Typography>
-      </div>
-    ),
-  },
-  {
-    header: "category",
-    cell: ({ row }) => (
-      <Typography
-        className={cn(
-          "block max-w-52 truncate",
-          !row.original.categories?.name && "pl-8"
-        )}
-      >
-        {row.original.categories?.name || "—"}
-      </Typography>
-    ),
-  },
-  {
-    header: "price",
-    cell: ({ row }) => {
-      return formatAmount(row.original.cost_price);
-    },
-  },
-  {
-    header: "sale price",
-    cell: ({ row }) => {
-      return formatAmount(row.original.selling_price);
-    },
-  },
-  {
-    header: "stock",
-    cell: ({ row }) => row.original.stock,
-  },
-  {
-    header: "status",
-    cell: ({ row }) => {
-      const stock = row.original.stock;
-      const status = stock > 0 ? "selling" : "out-of-stock";
-
-      return (
-        <Badge
-          variant={ProductBadgeVariants[status]}
-          className="flex-shrink-0 text-xs"
-        >
-          {status === "selling" ? "Selling" : "Out of stock"}
-        </Badge>
-      );
-    },
-  },
-  {
-    header: "view",
-    cell: ({ row }) => (
-      <Button size="icon" asChild variant="ghost" className="text-foreground">
-        <Link href={`/products/${row.original.slug}`}>
-          <ZoomIn className="size-5" />
-        </Link>
-      </Button>
-    ),
-  },
-  {
-    header: "published",
-    cell: ({ row }) => (
-      <div className="pl-5">
-        <TableSwitch
-          checked={row.original.published}
-          toastSuccessMessage="Product status updated successfully."
-          queryKey="products"
-          onCheckedChange={() =>
-            toggleProductPublishedStatus(
-              row.original.id,
-              row.original.published
-            )
-          }
-        />
-      </div>
-    ),
-  },
-  {
-    header: "actions",
-    cell: ({ row }) => {
-      return (
-        <div className="flex items-center gap-1">
-          <ProductFormSheet
-            key={row.original.id}
-            title="Update Products"
-            description="Update necessary product information here"
-            submitButtonText="Update Product"
-            actionVerb="updated"
-            initialData={{
-              name: row.original.name,
-              description: row.original.description ?? "",
-              image: row.original.image_url,
-              sku: row.original.sku,
-              category: row.original.category_id,
-              costPrice: row.original.cost_price,
-              salesPrice: row.original.selling_price,
-              stock: row.original.stock,
-              minStockThreshold: row.original.min_stock_threshold,
-              slug: row.original.slug,
-            }}
-            action={(formData) => editProduct(row.original.id, formData)}
-            previewImage={row.original.image_url}
-          >
-            <SheetTooltip content="Edit Product">
-              <PenSquare className="size-5" />
-            </SheetTooltip>
-          </ProductFormSheet>
-
-          <TableActionAlertDialog
-            title={`Delete ${row.original.name}?`}
-            description="This action cannot be undone. This will permanently delete the product and its associated data from the database."
-            tooltipContent="Delete Product"
-            actionButtonText="Delete Product"
-            toastSuccessMessage={`Product "${row.original.name}" deleted successfully!`}
-            queryKey="products"
-            action={() => deleteProduct(row.original.id)}
-          >
-            <Trash2 className="size-5" />
-          </TableActionAlertDialog>
+          <Typography className="capitalize block truncate">
+            {row.original.name}
+          </Typography>
         </div>
-      );
+      ),
     },
-  },
-];
+    {
+      header: "category",
+      cell: ({ row }) => (
+        <Typography
+          className={cn(
+            "block max-w-52 truncate",
+            !row.original.categories?.name && "pl-8"
+          )}
+        >
+          {row.original.categories?.name || "—"}
+        </Typography>
+      ),
+    },
+    {
+      header: "price",
+      cell: ({ row }) => {
+        return formatAmount(row.original.cost_price);
+      },
+    },
+    {
+      header: "sale price",
+      cell: ({ row }) => {
+        return formatAmount(row.original.selling_price);
+      },
+    },
+    {
+      header: "stock",
+      cell: ({ row }) => row.original.stock,
+    },
+    {
+      header: "status",
+      cell: ({ row }) => {
+        const stock = row.original.stock;
+        const status = stock > 0 ? "selling" : "out-of-stock";
+
+        return (
+          <Badge
+            variant={ProductBadgeVariants[status]}
+            className="flex-shrink-0 text-xs"
+          >
+            {status === "selling" ? "Selling" : "Out of stock"}
+          </Badge>
+        );
+      },
+    },
+    {
+      header: "view",
+      cell: ({ row }) => (
+        <Button size="icon" asChild variant="ghost" className="text-foreground">
+          <Link href={`/products/${row.original.slug}`}>
+            <ZoomIn className="size-5" />
+          </Link>
+        </Button>
+      ),
+    },
+  ];
+
+  if (hasPermission("products", "canTogglePublished")) {
+    columns.splice(7, 0, {
+      header: "published",
+      cell: ({ row }) => (
+        <div className="pl-5">
+          <TableSwitch
+            checked={row.original.published}
+            toastSuccessMessage="Product status updated successfully."
+            queryKey="products"
+            onCheckedChange={() =>
+              toggleProductPublishedStatus(
+                row.original.id,
+                row.original.published
+              )
+            }
+          />
+        </div>
+      ),
+    });
+  }
+
+  if (
+    hasPermission("products", "canDelete") ||
+    hasPermission("products", "canEdit")
+  ) {
+    columns.splice(0, 0, {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+    });
+
+    columns.splice(9, 0, {
+      header: "actions",
+      cell: ({ row }) => {
+        return (
+          <div className="flex items-center gap-1">
+            {hasPermission("products", "canEdit") && (
+              <ProductFormSheet
+                key={row.original.id}
+                title="Update Products"
+                description="Update necessary product information here"
+                submitButtonText="Update Product"
+                actionVerb="updated"
+                initialData={{
+                  name: row.original.name,
+                  description: row.original.description ?? "",
+                  image: row.original.image_url,
+                  sku: row.original.sku,
+                  category: row.original.category_id,
+                  costPrice: row.original.cost_price,
+                  salesPrice: row.original.selling_price,
+                  stock: row.original.stock,
+                  minStockThreshold: row.original.min_stock_threshold,
+                  slug: row.original.slug,
+                }}
+                action={(formData) => editProduct(row.original.id, formData)}
+                previewImage={row.original.image_url}
+              >
+                <SheetTooltip content="Edit Product">
+                  <PenSquare className="size-5" />
+                </SheetTooltip>
+              </ProductFormSheet>
+            )}
+
+            {hasPermission("products", "canDelete") && (
+              <TableActionAlertDialog
+                title={`Delete ${row.original.name}?`}
+                description="This action cannot be undone. This will permanently delete the product and its associated data from the database."
+                tooltipContent="Delete Product"
+                actionButtonText="Delete Product"
+                toastSuccessMessage={`Product "${row.original.name}" deleted successfully!`}
+                queryKey="products"
+                action={() => deleteProduct(row.original.id)}
+              >
+                <Trash2 className="size-5" />
+              </TableActionAlertDialog>
+            )}
+          </div>
+        );
+      },
+    });
+  }
+
+  return columns;
+};
 
 export const skeletonColumns: SkeletonColumn[] = [
   {
