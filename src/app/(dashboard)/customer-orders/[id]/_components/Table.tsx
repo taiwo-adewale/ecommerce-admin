c5e5223c194e8db813a6b3dcd87f214dbdc0f4e5
Taiwo Adewale <adewaletaiwo08@gmail.com>
2025-08-26 05:34:54 +0100

    add RBAC for customer orders table features

--- src/app/(dashboard)/customer-orders/[id]/_components/Table.tsx
+++ src/app/(dashboard)/customer-orders/[id]/_components/Table.tsx
@@ -2,14 +2,18 @@
 
 import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
 
-import { columns } from "./columns";
+import { getColumns } from "./columns";
 import DataTable from "@/components/shared/table/DataTable";
 import { DataTableProps } from "@/types/data-table";
 import { CustomerOrder } from "@/services/customers/types";
+import { useAuthorization } from "@/hooks/use-authorization";
 
 export default function CustomerOrdersTable({
   data,
 }: Omit<DataTableProps<CustomerOrder>, "columns" | "pagination">) {
+  const { hasPermission } = useAuthorization();
+  const columns = getColumns({ hasPermission });
+
   const table = useReactTable({
     data,
     columns,
--- src/app/(dashboard)/customer-orders/[id]/_components/columns.tsx
+++ src/app/(dashboard)/customer-orders/[id]/_components/columns.tsx
@@ -13,79 +13,92 @@ import { OrderStatus } from "@/services/orders/types";
 import { CustomerOrder } from "@/services/customers/types";
 
 import { changeOrderStatus } from "@/actions/orders/changeOrderStatus";
+import { HasPermission } from "@/hooks/use-authorization";
 
-export const columns: ColumnDef<CustomerOrder>[] = [
-  {
-    header: "invoice no",
-    cell: ({ row }) => row.original.invoice_no,
-  },
-  {
-    header: "order time",
-    cell: ({ row }) =>
-      `${format(row.original.order_time, "PP")} ${format(
-        row.original.order_time,
-        "p"
-      )}`,
-  },
-  {
-    header: "shipping address",
-    cell: ({ row }) => (
-      <span className="block max-w-72 truncate">
-        {row.original.customers?.address}
-      </span>
-    ),
-  },
-  {
-    header: "phone",
-    cell: ({ row }) => (
-      <Typography className={cn(!row.original.customers.phone && "pl-6")}>
-        {row.original.customers.phone || "—"}
-      </Typography>
-    ),
-  },
-  {
-    header: "method",
-    cell: ({ row }) => (
-      <span className="capitalize">{row.original.payment_method}</span>
-    ),
-  },
-  {
-    header: "amount",
-    cell: ({ row }) => formatAmount(row.original.total_amount),
-  },
-  {
-    header: "status",
-    cell: ({ row }) => {
-      const status = row.original.status;
+export const getColumns = ({
+  hasPermission,
+}: {
+  hasPermission: HasPermission;
+}) => {
+  const columns: ColumnDef<CustomerOrder>[] = [
+    {
+      header: "invoice no",
+      cell: ({ row }) => row.original.invoice_no,
+    },
+    {
+      header: "order time",
+      cell: ({ row }) =>
+        `${format(row.original.order_time, "PP")} ${format(
+          row.original.order_time,
+          "p"
+        )}`,
+    },
+    {
+      header: "shipping address",
+      cell: ({ row }) => (
+        <span className="block max-w-72 truncate">
+          {row.original.customers?.address}
+        </span>
+      ),
+    },
+    {
+      header: "phone",
+      cell: ({ row }) => (
+        <Typography className={cn(!row.original.customers.phone && "pl-6")}>
+          {row.original.customers.phone || "—"}
+        </Typography>
+      ),
+    },
+    {
+      header: "method",
+      cell: ({ row }) => (
+        <span className="capitalize">{row.original.payment_method}</span>
+      ),
+    },
+    {
+      header: "amount",
+      cell: ({ row }) => formatAmount(row.original.total_amount),
+    },
+    {
+      header: "status",
+      cell: ({ row }) => {
+        const status = row.original.status;
 
-      return (
-        <Badge
-          variant={OrderBadgeVariants[status]}
-          className="flex-shrink-0 text-xs capitalize"
-        >
-          {status}
-        </Badge>
-      );
+        return (
+          <Badge
+            variant={OrderBadgeVariants[status]}
+            className="flex-shrink-0 text-xs capitalize"
+          >
+            {status}
+          </Badge>
+        );
+      },
     },
-  },
-  {
-    header: "action",
-    cell: ({ row }) => {
-      return (
-        <TableSelect
-          value={row.original.status}
-          toastSuccessMessage="Order status updated successfully."
-          queryKey="orders"
-          onValueChange={(value) =>
-            changeOrderStatus(row.original.id, value as OrderStatus)
-          }
-        >
-          <SelectItem value="pending">Pending</SelectItem>
-          <SelectItem value="processing">Processing</SelectItem>
-          <SelectItem value="delivered">Delivered</SelectItem>
-          <SelectItem value="cancelled">Cancelled</SelectItem>
-        </TableSelect>
-      );
-    },
-  },
-];
+  ];
+
+  if (hasPermission("orders", "canChangeStatus"))
+    [
+      columns.push({
+        header: "action",
+        cell: ({ row }) => {
+          return (
+            <TableSelect
+              value={row.original.status}
+              toastSuccessMessage="Order status updated successfully."
+              queryKey="orders"
+              onValueChange={(value) =>
+                changeOrderStatus(row.original.id, value as OrderStatus)
+              }
+            >
+              <SelectItem value="pending">Pending</SelectItem>
+              <SelectItem value="processing">Processing</SelectItem>
+              <SelectItem value="delivered">Delivered</SelectItem>
+              <SelectItem value="cancelled">Cancelled</SelectItem>
+            </TableSelect>
+          );
+        },
+      }),
+    ];
+
+  return columns;
+};
